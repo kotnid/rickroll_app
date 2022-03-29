cfb5227486639a53c74a7c0107a3f5a3da5c2f7b
kotnid <tkt0506tkt@gmail.com>
2022-03-29 13:57:23 +0800

    my_java_app_2 : Finish app
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- app/src/main/java/com/example/my_java_app_2/MainActivity.java
+++ app/src/main/java/com/example/my_java_app_2/MainActivity.java
@@ -2,15 +2,63 @@ package com.example.my_java_app_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.view.View;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    AudioManager AudioManager;
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        AudioManager = (AudioManager)getSystemService(this.AUDIO_SERVICE);
+
+
+        AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+        player = MediaPlayer.create(this , R.raw.audio);
+        player.start();
+
+
+        //Toast.makeText(this , "Volume : " + AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) , Toast.LENGTH_SHORT);
+
+
+
+        //player = MediaPlayer.create(this , R.raw.audio);
+        //player.start();
+
+    }
+    public void onclick (View view){
+        TextView txt = findViewById(R.id.test);
+        txt.setText("Volume " + AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
     }
 
+    public void onclick2 (View view){
+        AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+        player = MediaPlayer.create(this , R.raw.audio);
+        player.start();
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+            default:
+                return super.dispatchKeyEvent(event);
+        }
+    }
 }
