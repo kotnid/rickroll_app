37c3880e9beb88cec606880bb56b74decd7e5558
kotnid <tkt0506tkt@gmail.com>
2022-04-01 17:10:42 +0800

    rickroll_app : Switch to andriod 13 updater
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- app/src/main/java/com/example/my_java_app_2/MainActivity.java
+++ app/src/main/java/com/example/my_java_app_2/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.my_java_app_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 import android.widget.TextView;
 import android.view.View;
 import android.view.KeyEvent;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +35,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        TextView txtview2 = findViewById(R.id.textView2);
+        txtview2.setText(android.os.Build.MODEL + " can upgrade");
         AudioManager = (AudioManager)getSystemService(this.AUDIO_SERVICE);
 
 
-        AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+        AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)-5, 0);
         player = MediaPlayer.create(this , R.raw.audio);
         player.start();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("notify" , "notify" , NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = this.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this , "notify")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle("rickrolled")
+                .setContentText("shhh don't spoil it to others :)")
+                .setSmallIcon(R.raw.icon);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1 , builder.build());
 
 
         //Toast.makeText(this , "Volume : " + AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) , Toast.LENGTH_SHORT);
@@ -40,14 +72,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onclick (View view){
-        TextView txt = findViewById(R.id.test);
-        txt.setText("Volume " + AudioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
-    }
+        player.stop();
 
-    public void onclick2 (View view){
-        AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
-        player = MediaPlayer.create(this , R.raw.audio);
-        player.start();
+        Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+        httpIntent.setData(Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+
+        startActivity(httpIntent);
+
+
     }
 
     @Override
@@ -56,9 +88,10 @@ public class MainActivity extends AppCompatActivity {
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+                AudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)-5, 0);
             default:
                 return super.dispatchKeyEvent(event);
         }
     }
 }
+
